a87d83d81f5240741ad1990867bfe1e11ea2b165
Pratik Napit <pratiknapit@Pratiks-MacBook-Pro.local>
2026-05-14 16:19:30 +1000

    Untracked files

--- lab5/Part1/1second_counter_fpga.v
+++ lab5/Part1/1second_counter_fpga.v
@@ -6,8 +6,8 @@ module 1second_counter(CLK_50, HEX1, HEX2);
     wire sec_1;
     wire[3:0] 4bit_Q;
 
-    counter_26bit counter_26(.clk(CLK_50), .reset(), .sec_1(sec_1));
-    counter_4bit counter4(.clk(sec_1), .reset(), .Q(4bit_Q));
+    counter_26bit counter_26(.clk(CLK_50), .reset(KEY0), .sec_1(sec_1));
+    counter_4bit counter4(.clk(sec_1), .reset(KEY0), .Q(4bit_Q));
     4bit_decoder_7seg display(.code(4bit_Q), .decoded_output1(HEX1), .decoded_output2(HEX2));
 
 endmodule
